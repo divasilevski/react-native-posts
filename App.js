e67933bcf7433ac1130a603ef3917da4985105a4
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-05-27 14:11:12 +0300

    Добавлена загрузка шрифтов

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return <AppLoading
+      startAsync={bootstrap}
+      onFinish={() => setIsReady(true)}
+      onError={console.log}
+    />
+  }
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
