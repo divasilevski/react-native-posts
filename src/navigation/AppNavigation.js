32c3367e1942d7ca01451ad17e7aa2c3932340b2
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-05-30 20:32:21 +0300

    Добавлена таб навигация

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { BookmarkScreen } from '../screens/BookmarkScreen';
 import { THEME } from '../theme';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 const PostNavigator = createStackNavigator(
   {
@@ -16,12 +20,52 @@ const PostNavigator = createStackNavigator(
     initialRouteName: 'Main',
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor:
-          Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
       },
       headerTintColor: Platform.OS !== 'android' ? THEME.MAIN_COLOR : 'white',
     },
   }
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookmarkScreen,
+    Post: PostScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
+      },
+      headerTintColor: Platform.OS !== 'android' ? THEME.MAIN_COLOR : 'white',
+    },
+  }
+);
+
+const BottomNavigator = createBottomTabNavigator(
+  {
+    Post: {
+      screen: PostNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+    Booked: {
+      screen: BookedNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-star" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: THEME.MAIN_COLOR,
+    },
+  }
+);
+
+export const AppNavigation = createAppContainer(BottomNavigator);
