95bc62af15cb63fefa61b9661f0221c7ba2217a1
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-05-30 19:02:20 +0300

    Страница поста

--- src/screens/PostScreen.jsx
+++ src/screens/PostScreen.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import { DATA } from '../data';
+import { THEME } from '../theme';
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam('postId');
+
+  const post = DATA.find((p) => p.id === postId);
+
+  const removeHandler = () => {
+    Alert.alert('Удаление поста', 'Вы уверены?', [
+      { text: 'Отменить', style: 'cancel' },
+      {
+        text: 'Удалить',
+        style: 'destructive',
+        onPress: () => console.log('pressed'),
+      },
+    ]),
+      { cancelable: false };
+  };
+
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: post.img }} />
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title="Удалить"
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   );
 };
 
@@ -19,9 +52,14 @@ PostScreen.navigationOptions = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: 'open-regular',
   },
 });
