fcc58b1acfb97d7473103bde9d23c36c91aa065a
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-06-01 17:01:15 +0300

    Страница добавления поста

--- src/screens/CreateScreen.jsx
+++ src/screens/CreateScreen.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import * as React from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { AppHeaderIcon } from '../components/AppHeaderIcon';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  Button,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import { THEME } from '../theme';
 
 export const CreateScreen = ({}) => {
+  const [text, setText] = React.useState('');
+  const saveHandler = () => {};
   return (
-    <View style={styles.center}>
-      <Text>Create Screen</Text>
-    </View>
+    <ScrollView>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Новый пост</Text>
+          <TextInput
+            style={styles.textArea}
+            value={text}
+            onChangeText={setText}
+            placeholder="Введите текст поста"
+            multiline
+          />
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+            }}
+          />
+          <Button
+            title="Создать"
+            color={THEME.MAIN_COLOR}
+            onPress={saveHandler}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
@@ -25,9 +64,21 @@ CreateScreen.navigationOptions = ({ navigation }) => ({
 });
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  wrapper: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'open-regular',
+    marginVertical: 10,
+  },
+  textArea: {
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
   },
 });
