5a7a370b2424d44261071b3bc0b33b18e64851b9
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-06-01 13:46:43 +0300

    Добавлены закладки

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
 import { BookmarkScreen } from '../screens/BookmarkScreen';
@@ -9,22 +10,21 @@ import { THEME } from '../theme';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
+const navigatorOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
+    },
+    headerTintColor: Platform.OS !== 'android' ? THEME.MAIN_COLOR : 'white',
+  },
+};
+
 const PostNavigator = createStackNavigator(
   {
     Main: MainScreen,
-    Post: {
-      screen: PostScreen,
-    },
+    Post: PostScreen,
   },
-  {
-    initialRouteName: 'Main',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
-      },
-      headerTintColor: Platform.OS !== 'android' ? THEME.MAIN_COLOR : 'white',
-    },
-  }
+  navigatorOptions
 );
 
 const BookedNavigator = createStackNavigator(
@@ -32,40 +32,44 @@ const BookedNavigator = createStackNavigator(
     Booked: BookmarkScreen,
     Post: PostScreen,
   },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : 'white',
-      },
-      headerTintColor: Platform.OS !== 'android' ? THEME.MAIN_COLOR : 'white',
-    },
-  }
+  navigatorOptions
 );
 
-const BottomNavigator = createBottomTabNavigator(
-  {
-    Post: {
-      screen: PostNavigator,
-      navigationOptions: {
-        tabBarIcon: (info) => (
-          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
-        ),
-      },
-    },
-    Booked: {
-      screen: BookedNavigator,
-      navigationOptions: {
-        tabBarIcon: (info) => (
-          <Ionicons name="ios-star" size={25} color={info.tintColor} />
-        ),
-      },
+const bottomTabsConfig = {
+  Post: {
+    screen: PostNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Все',
+      tabBarIcon: (info) => (
+        <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+      ),
     },
   },
-  {
-    tabBarOptions: {
-      activeTintColor: THEME.MAIN_COLOR,
+  Booked: {
+    screen: BookedNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Избранное',
+      tabBarIcon: (info) => (
+        <Ionicons name="ios-star" size={25} color={info.tintColor} />
+      ),
     },
-  }
-);
+  },
+};
+
+const BottomNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: '#fff',
+        shifting: true,
+        barStyle: {
+          backgroundColor: THEME.MAIN_COLOR,
+        },
+      })
+    : createBottomTabNavigator({
+        bottomTabsConfig,
+        tabBarOptions: {
+          activeTintColor: THEME.MAIN_COLOR,
+        },
+      });
 
 export const AppNavigation = createAppContainer(BottomNavigator);
--- src/screens/BookmarkScreen.jsx
+++ src/screens/BookmarkScreen.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
+import { DATA } from '../data';
+import { PostList } from '../components/PostList';
 
-export const BookmarkScreen = ({}) => {
-  return (
-    <View style={styles.center}>
-      <Text>Bookmark Screen</Text>
-    </View>
-  );
+export const BookmarkScreen = ({ navigation }) => {
+  const openPostHandler = (post) => {
+    navigation.navigate('Post', {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked,
+    });
+  };
+
+  const data = DATA.filter((post) => post.booked);
+  return <PostList data={data} onOpen={openPostHandler} />;
 };
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+BookmarkScreen.navigationOptions = {
+  headerTitle: 'Избранное',
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Toggle drawer"
+        iconName="ios-menu"
+        onPress={() => console.log('press photo')}
+      />
+    </HeaderButtons>
+  ),
+};
